95b787aaf86fd6a5872d3045a47198b512c415e1
AnaVox6y <ana_35an@yahoo.ca>
2018-01-28 23:18:13 -0500

    update code

---
+++ app/src/main/java/annin/my/android/bakingapp/fragments/IngredientsFragment.java
@@ -0,0 +1,8 @@
+package annin.my.android.bakingapp.fragments;
+
+/**
+ * Created by Maino96-10022 on 1/23/2018.
+ */
+
+public class IngredientsFragment {
+}
---
+++ app/src/main/java/annin/my/android/bakingapp/recyclerviewadapters/IngredientsAdapter.java
@@ -0,0 +1,8 @@
+package annin.my.android.bakingapp.recyclerviewadapters;
+
+/**
+ * Created by Maino96-10022 on 1/25/2018.
+ */
+
+public class IngredientsAdapter {
+}
