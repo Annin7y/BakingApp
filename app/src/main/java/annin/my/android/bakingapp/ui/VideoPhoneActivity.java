5c290e86982ae9245b1c3e2f287ff36719218a7d
AnaVox6y <ana_35an@yahoo.ca>
2018-07-15 23:47:36 -0400

    update code: refactor

---
+++ app/src/main/java/annin/my/android/bakingapp/ui/VideoPhoneActivity.java
@@ -0,0 +1,18 @@
+package annin.my.android.bakingapp.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import annin.my.android.bakingapp.R;
+
+public class VideoPhoneActivity extends AppCompatActivity {
+
+    private static final String TAG = VideoPhoneActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videophone);
+        
+    }
+}
