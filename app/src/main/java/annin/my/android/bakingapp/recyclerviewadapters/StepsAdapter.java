e13afe4815c1a18408a4985d031ddd2714064608
AnaVox6y <ana_35an@yahoo.ca>
2018-08-02 00:12:05 -0400

    update code: refactor

--- app/src/main/java/annin/my/android/bakingapp/recyclerviewadapters/StepsAdapter.java
+++ app/src/main/java/annin/my/android/bakingapp/recyclerviewadapters/StepsAdapter.java
@@ -86,7 +86,15 @@ public class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.StepsAdapter
 
         holder.stepShortDescription.setText(stepsView.getStepShortDescription());
         holder.stepDescription.setText(stepsView.getStepDescription());
-    }
+        holder.btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // use callback function in the place you want
+                mListener.onClickAtOKButton(position);
+            }
+        });
+
+        }
 
     @Override
     public int getItemCount()
