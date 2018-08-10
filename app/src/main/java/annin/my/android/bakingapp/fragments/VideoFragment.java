56a6b767083e74513ed4f88614fbbdd09bb30528
AnaVox6y <ana_35an@yahoo.ca>
2018-08-10 04:11:00 -0400

    update code: refactor

---
+++ app/src/main/java/annin/my/android/bakingapp/fragments/VideoFragment.java
@@ -0,0 +1,31 @@
+package annin.my.android.bakingapp.fragments;
+
+public class VideoFragment {
+
+    private static final String TAG = VideoFragment.class.getSimpleName();
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the fragment
+     */
+    public VideoFragment() {
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+            releasePlayer();
+        }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+            releasePlayer();
+        }
+    }
+
+
+
