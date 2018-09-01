bd41fb86bc313332a5fbd68ccfeda26606d2e688
AnaVox6y <ana_35an@yahoo.ca>
2018-08-31 21:57:16 -0400

    update code: refactor

--- app/src/main/java/annin/my/android/bakingapp/fragments/VideoFragment.java
+++ app/src/main/java/annin/my/android/bakingapp/fragments/VideoFragment.java
@@ -1,9 +1,18 @@
 package annin.my.android.bakingapp.fragments;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 
-public class VideoFragment {
+import annin.my.android.bakingapp.R;
+import butterknife.BindView;
+
+public class VideoFragment extends Fragment {
 
     private static final String TAG = VideoFragment.class.getSimpleName();
 
@@ -12,8 +21,19 @@ public class VideoFragment {
      */
     public VideoFragment() {
     }
+
     private SimpleExoPlayer mExoPlayer;
     private SimpleExoPlayerView mPlayerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Inflate the Steps fragment layout
+        View rootView = inflater.inflate(R.layout.fragment_video, container, false);
+
+        mPlayerView = (SimpleExoPlayerView) findViewById(R.id.playerView);
+
+
+
 
 //    @Override
 //    public void onPause() {
