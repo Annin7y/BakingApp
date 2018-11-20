02576237a23c5ae1d26432d9fe599a7e516e6292
AnaVox6y <ana_35an@yahoo.ca>
2018-11-19 20:36:10 -0500

    update code: refactor

--- app/src/main/java/annin/my/android/bakingapp/fragments/VideoFragment.java
+++ app/src/main/java/annin/my/android/bakingapp/fragments/VideoFragment.java
@@ -71,8 +71,8 @@ public class VideoFragment extends Fragment
             stepClick= getArguments().getParcelable("Steps");
                 if(stepClick != null) {
                     videoUrl = stepClick.getVideoUrl();
-                   // videoUrl_Parse = Uri.parse(videoUrl);
-                   // initializePlayer(videoUrl_Parse);
+                   videoUrl_Parse = Uri.parse(videoUrl);
+                   initializePlayer(videoUrl_Parse);
 
                     thumbnailUrl = stepClick.getThumbnailUrl();
                     thumbnailUrl_Parse = Uri.parse(thumbnailUrl);
@@ -110,13 +110,14 @@ public class VideoFragment extends Fragment
     {
         super.onStart();
         if (Util.SDK_INT <= 23) {
-            videoUrl_Parse = Uri.parse(videoUrl);
-            initializePlayer(videoUrl_Parse);
 
-            if(thumbnailUrl != null) {
-                Picasso.with(getContext())
-                        .load(thumbnailUrl_Parse).into(thumbnailUrlImage);
-            }
+          //  videoUrl_Parse = Uri.parse(videoUrl);
+           // initializePlayer(videoUrl_Parse);
+
+         //   if(thumbnailUrl != null) {
+           //     Picasso.with(getContext())
+             //           .load(thumbnailUrl_Parse).into(thumbnailUrlImage);
+           // }
         }
     }
 
@@ -125,9 +126,10 @@ public class VideoFragment extends Fragment
     {
         super.onPause();
         if (Util.SDK_INT <= 23) {
+            if(mExoPlayer!= null){
             releasePlayer();
         }
-    }
+    }}
 
 
     @Override
