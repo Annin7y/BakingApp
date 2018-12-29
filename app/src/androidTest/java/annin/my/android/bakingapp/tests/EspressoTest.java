1fd4488fb7cc0939617aa02e9eb5f624eb10fa77
Maino96-10022 <anast3222@gmail.com>
2018-12-29 03:55:45 -0500

    update code: refactor

--- app/src/androidTest/java/annin/my/android/bakingapp/tests/EspressoTest.java
+++ app/src/androidTest/java/annin/my/android/bakingapp/tests/EspressoTest.java
@@ -3,12 +3,17 @@ package annin.my.android.bakingapp.tests;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.runner.RunWith;
+
+import annin.my.android.bakingapp.ui.MainActivity;
 
 @RunWith(AndroidJUnit4.class)
 public class EspressoTest
 {
 
+//    public ActivityTestRule<MainActivity> activityTestRule =
+//        new ActivityTestRule<>(MainActivity.class);
 
 
 }
