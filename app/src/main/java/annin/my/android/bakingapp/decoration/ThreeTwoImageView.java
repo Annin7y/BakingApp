37e686fdb3fa549885f5d13ece55856881d265f0
ANNIN_ST_PC <anast3222@gmail.com>
2020-01-30 23:57:41 -0500

    update code: add threetwoimageview

---
+++ app/src/main/java/annin/my/android/bakingapp/decoration/ThreeTwoImageView.java
@@ -0,0 +1,35 @@
+package annin.my.android.bakingapp.decoration;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+public class ThreeTwoImageView extends AppCompatImageView
+{
+    //code copied and pasted from:
+    //https://github.com/udacity/ud862-samples/blob/master/Unsplash/app/src/main/java/com/example/android/unsplash/ui/ThreeTwoImageView.java
+
+    public ThreeTwoImageView(Context context) {
+        super(context);
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int desiredHeight = width * 2 / 3;
+        super.onMeasure(widthMeasureSpec,
+                MeasureSpec.makeMeasureSpec(desiredHeight, MeasureSpec.EXACTLY));
+    }
+}
+
+
+
+
